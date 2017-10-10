034298706e2767c67370862408d07d83dfb5ed96
Joshua Eng <jpe4602@rit.edu>
2017-10-10 18:51:50 -0400

    Added textfiles with the "CSV" data, initials for Client/TextUI
    Copied over the starting text files, started the Observer pattern for Client, initialization stuff.

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Observable;
+
 /**
  * Created by melis on 10/5/2017.
  */
-public class Client {
+public class Client extends Observable
+{
+    private String S;
+
+    public void takeInput ()
+    {
+        S = "";
+        while(true)
+        {
+            System.out.println("Loop through to take in input");
+
+
+
+        }
+
+    }
+    public void setInput(String str)
+    {
+        this.S = str;
+    }
+    public String getInput()
+    {
+        return this.S;
+    }
 
 }
---
+++ src/TextUI.java
@@ -0,0 +1,18 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class TextUI implements Observer
+{
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to AFRS! \n");
+        System.out.println("In Development Now...");
+
+    }
+}
