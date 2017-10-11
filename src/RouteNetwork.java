1f57cdd5352d34ce0a6dcf6666e9d0a2f2daa383
Melissa Gould <mag8224@g.rit.edu>
2017-10-10 21:45:29 -0400

    Start of Route Network
    Started the intialization of RouteNetwork, still need to finish the reading in of files

--- src/RouteNetwork.java
+++ src/RouteNetwork.java
@@ -1,2 +1,65 @@
-public class RouteNetwork {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class RouteNetwork
+{
+    private HashMap routes = new HashMap();
+    private List<Flight> flights = new ArrayList<Flight>();
+    private List<Airport> airports = new ArrayList<Airport>();
+    private List<Reservation> reservations = new ArrayList<Reservation>();
+    private List<Itinerary> itineraries = new ArrayList<Itinerary>();
+
+
+    public RouteNetwork()
+    {
+
+    }
+
+    public void readInfo(File f)
+    {
+        try
+        {
+            BufferedReader fr = new BufferedReader(new FileReader(f));
+            String line = fr.readLine();
+            while(line != null)
+            {
+                System.out.println(line);
+                String[] args = line.split(",");
+                System.out.println(args[0] + " " + args[1]);
+                line = fr.readLine();
+
+            }
+            fr.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void storeFlight(Flight f)
+    {
+
+    }
+
+    public void storeAirport(Airport a)
+    {
+
+    }
+    public ArrayList<Itinerary> createItineraries(Airport origin, Airport destination)
+    {
+        return null;
+
+    }
+    public static void main(String[] args)
+    {
+        RouteNetwork r = new RouteNetwork();
+        File f = new File("C:\\Users\\melis\\Documents\\GitHub\\ARFS\\src\\inputFiles\\airports.txt");
+        r.readInfo(f);
+    }
+
 }
