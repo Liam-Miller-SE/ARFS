01898f8d425699c58db5ac0cfb782673003bf7d9
tzahald <tylerjdavis.96@gmail.com>
2017-10-12 14:48:26 -0400

    created HQuery

---
+++ src/HQuery.java
@@ -0,0 +1,28 @@
+public class HQuery
+{
+	public HQuery
+	{
+	}
+	public String query(String[] query)
+	{
+		if( query[0] == "Reservation")
+		{
+			ReservationQuery rq = new ReservationQuery() ;
+			return rq.getData(query[1:]) ;
+		else if( query[0] == "Airport")
+		{
+			AirportQuery aq = new AirportQuery() ;
+			return aq.getData(query[1:]) ;
+		}
+		else if( query[0] == "Flight")
+		{
+			FlightQuery fq = new FlightQuery() ;
+			return fq.getData(query[1:]) ;
+		}
+		else if( query[0] == "Itinerary")
+		{
+			ItineraryQuery iq = new ItineraryQuery() ;
+			return iq.getData(query[1:]) ;
+		}
+	}
+}
