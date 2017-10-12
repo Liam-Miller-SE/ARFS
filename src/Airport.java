6d8d529eec6d5953f51073c1fbf6325095e66d95
tzahald <tylerjdavis.96@gmail.com>
2017-10-12 09:07:18 -0400

    basic constructor

--- src/Airport.java
+++ src/Airport.java
@@ -1,3 +1,15 @@
 public class Airport 
 {
+	int code;
+	int temperature;
+	String weather; 
+	String city; 
+	
+	public Airport(int co, int temp, String weath, String cit)
+	{
+		code = co; 
+		temperature = temp;
+		weather = weath;
+		city = cit;
+	}
 }
