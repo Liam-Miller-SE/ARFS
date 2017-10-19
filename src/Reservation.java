d68192f2da672bd77eba7540383782ec2622f896
tzahald <tylerjdavis.96@gmail.com>
2017-10-19 14:36:31 -0400

    reservation
    reservation get methods and query started

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,10 +1,18 @@
 public class Reservation 
 {
-  Itinerary itinerary;
-  String passengerName;
+  private Itinerary itinerary;
+  private String passengerName;
   public Reservation(String name, Itinerary itin)
   {
     itinerary = itin;
     passengerName = name;
+  }
+  public getItinerary()
+  {
+	  return this.itinerary;
+  }
+  public getPassenger()
+  {
+	  return this.passengerName;
   }
 }
--- src/ReservationQuery.java
+++ src/ReservationQuery.java
@@ -5,6 +5,22 @@ public class ReservationQuery implements IQuery
 {
 	public String processData(String[] query)
 	{
+		String pass = query[0];
+		RouteNetwork rn = RouteNetwork.getInstance() ;
+		List<Reservation> r = rn.getReservations();
+		List<Reservation> pmatch = new ArrayList<Reservation>();
+		for(int i = 0; i < r.size(); i++)
+		{
+			if(r.get(i).getPassenger() == pass)
+			{
+				pmatch.add(r.get(i));
+			}
+		}
+		/**if (query.length == 1)
+		{
+			
+			
+		}*/
 		//how to query by length of query passed in
 		return null;
 	}
