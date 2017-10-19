876d1f9d1f277cd131825b0b2fd9cbf3e77b0a5a
Joshua Eng <jpe4602@rit.edu>
2017-10-19 14:01:53 -0400

    Loads in Files, Cleaning up Client
    Uses Observer Pattern over simple system print lines, bug fixing with parameters

--- src/TextUI.java
+++ src/TextUI.java
@@ -7,7 +7,7 @@ import java.util.Observer;
 public class TextUI implements Observer
 {
     private Client c;
-    private ArrayList<String> Files;
+    private static ArrayList<String> Files;
     private File f;
 
 
@@ -24,31 +24,32 @@ public class TextUI implements Observer
         String output = c.getInput();
         System.out.println(output);
     }
-    /**
-    public void getFiles()
+
+    public static void getFiles()
     {
         Files = new ArrayList<String>();
-        Files.add("weather.txt");
         Files.add("airports.txt");
+        Files.add("weather.txt");
         Files.add("connections.txt");
         Files.add("delays.txt");
         Files.add("flights.txt");
     }
 
-    public void loadFiles()
+    public static void loadFiles()
     {
+        RouteNetwork rn = RouteNetwork.getInstance();
+
         for(int i = 0; i < Files.size(); i ++)
         {
             File f = new File("src/inputFiles/" + Files.get(i) );
-            readInfo(f);
+            rn.readInfo(f);
         }
     }
-    **/
+
     public static void main(String[] args)
     {
-        RouteNetwork rn = RouteNetwork.getInstance();
-        //rn.getFiles();
-        //rn.loadFiles();
+        getFiles();
+        loadFiles();
         System.out.println("Welcome to AFRS!");
         System.out.println("We are now in the first release of development...");
         System.out.println("What are you looking to do?");
