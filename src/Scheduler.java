fc1e2dc25cb0ffb627a12f7c362d3da384a9c087
Melissa Gould <mag8224@g.rit.edu>
2017-11-08 09:43:55 -0500

    Added Undo/Redo
    Undo/Redo functionality

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,12 +1,20 @@
 //package ARFS;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Stack;
 
 /**
  * Created by melis on 10/5/2017.
  */
 public class Scheduler
 {
+
+
+	private Stack<Pair> undo;
+	private Stack<Pair> redo;
 	RouteNetwork rn;
 	public Scheduler()
 	{
@@ -45,7 +53,7 @@ public class Scheduler
 	 * Delete Reservation - take in a reservation and remove it from the
 	 * RouteNetwork sends a success message
 	 */
-	public int deleteReservation(String name, Airport org, Airport dest)
+	public int deleteReservation(Integer cid, String name, Airport org, Airport dest)
 	{
 		int error = 1;
 
@@ -56,11 +64,121 @@ public class Scheduler
 					r.getItinerary().getOrigin().getCode().equals(org.getCode())
 					&& r.getItinerary().getDestination().getCode().equals(dest.getCode()))
 			{
+
+				addElementUndo(cid, r);
 				rn.deleteReservation(r);
 				error = 0;
 			}
 		}
 		return error;
+
+	}
+
+	public void addElementUndo(Integer id, Reservation r)
+	{
+		Pair<Integer, Reservation> ele = new Pair<>(id, r);
+		undo.push(ele);
+		removeIdElements(id);
+	}
+
+	public void removeIdElements(Integer id)
+	{
+		Iterator<Pair> itr = redo.iterator();
+		int count =0;
+		while(itr.hasNext())
+		{
+			Pair<Integer, Reservation> temp = itr.next();
+			if(temp.getKey().equals(id))
+			{
+				redo.remove(count);
+			}
+			else{
+				count++;
+			}
+		}
+	}
+	public void addElementRedo(Pair<Integer, Reservation> ele)
+	{
+		redo.push(ele);
+	}
+
+	public void undo(Integer id) {
+		//clear redo stack before you undo an item
+
+		Stack<Pair> temp = new Stack<>();
+
+		boolean check = true;
+		while(check)
+		{
+			Pair<Integer, Reservation> ele = undo.pop();
+			if (ele.getKey().equals(id))
+			{
+				//See if the reservation is in the route network
+				//if it is then remove it from rn else add it
+				boolean inRN = false;
+				for(Reservation r: rn.getReservations())
+				{
+					if(r.getPassenger().equals(ele.getValue().getPassenger()) &&
+							r.getItinerary().getOrigin().getCode().equals(ele.getValue().getItinerary().getOrigin().getCode())
+							&& r.getItinerary().getDestination().getCode().equals(ele.getValue().getItinerary().getDestination().getCode()))
+					{
+						rn.deleteReservation(r);
+						inRN = true;
+					}
+				}
+				if(!inRN)
+				{
+					rn.storeReservation(ele.getValue());
+				}
+
+				addElementRedo(ele);
+				check = false;
+			}
+			else
+			{
+				temp.push(ele);
+			}
+
+		}
+		while(temp.size() != 0)
+		{
+			undo.push(temp.pop());
+		}
+	}
+	public void redo(Integer id)
+	{
+		Stack<Pair> temp = new Stack<>();
+		Pair<Integer, Reservation> element = redo.pop();
+		while(!element.getKey().equals(id) && redo.size() != 0)
+		{
+			temp.push(element);
+			element = redo.pop();
+		}
+		if(redo.size()!=0)
+		{
+			//check reservations in routenetwork for this reservation
+			boolean inRN = false;
+			for(Reservation r : rn.getReservations())
+			{
+				if(r.getPassenger().equals(element.getValue().getPassenger())
+						&& r.getItinerary().getOrigin().getCode().equals(element.getValue().getItinerary().getOrigin().getCode())
+						&& r.getItinerary().getDestination().getCode().equals(element.getValue().getItinerary().getDestination().getCode()))
+					{
+						rn.deleteReservation(r);
+						inRN = true;
+					}
+
+			}
+			if(!inRN)
+			{
+				rn.storeReservation(element.getValue());
+			}
+
+		}
+		while(temp.size() !=0)
+		{
+			redo.push(temp.pop());
+		}
 
 	}
 
